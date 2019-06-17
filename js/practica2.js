ff8acc6c84075d0c96fda6769518270bae2ca6ed
Alejandro-Magno <xcristopher335@gmail.com>
2019-06-17 09:25:32 -0400

    retoques finales

--- js/practica2.js
+++ js/practica2.js
@@ -31,7 +31,7 @@ class rubro {
 
     }
 
-   
+
 
 }
 
@@ -42,36 +42,86 @@ let pasta = new rubro('pasta', 1, 12, 13);
 let harina = new rubro('harina', 1, 22, 23);
 let lentejas = new rubro('lentejas', 1, 32, 13);
 
+let quesoBlanco = new rubro('quesoBlanco', 12, 12, 143);
+let mantequilla = new rubro('mantequilla', 1, 12, 43);
+let fiambre = new rubro('fiambre', 14, 11, 73);
+let jamonPavo = new rubro('jamon de Pavo', 17, 12, 93);
+
+let chocolate = new rubro('chocolate', 1, 22, 23);
+let galletas = new rubro('galletas', 1, 12, 11);
+let gomitas = new rubro('gomitas', 1, 14, 8);
+let caramelos = new rubro('caramelos', 1, 19, 3);
 
 
-function  clickaction(b) {
 
-    objetoId=b.id;
+function clickaction(b) {
 
-  
+    objetoId = b.id;
+
+
 
     switch (b.id) {
-
-        case 'lentejas':     
-           lentejas.escribir();
+        
+        //Cereales
+        case 'lentejas':
+            lentejas.escribir();
             break;
 
-            case 'arroz':     
-           arroz.escribir();
+        case 'arroz':
+            arroz.escribir();
             break;
 
         case 'harina':
-                harina.escribir();
+            harina.escribir();
             break;
 
 
         case 'pasta':
-                pasta.escribir();
+            pasta.escribir();
+            break;
+
+
+            //charcuteria
+
+        case 'quesoBlanco':
+            quesoBlanco.escribir();
+            break;
+
+        case 'mantequilla':
+            mantequilla.escribir();
+            break;
+
+        case 'fiambre':
+            fiambre.escribir();
+            break;
+
+
+        case 'jamonPavo':
+            jamonPavo.escribir();
             break;
 
 
         default:
             alert("no son lentejas");
+
+            //Chucherias
+
+            case 'chocolate':
+            chocolate.escribir();
+            break;
+
+        case 'galletas':
+            galletas.escribir();
+            break;
+
+        case 'gomitas':
+            gomitas.escribir();
+            break;
+
+
+        case 'caramelos':
+            caramelos.escribir();
+            break;
 
 
     }
@@ -81,7 +131,3 @@ function  clickaction(b) {
 
 
 // var elemento = document.getElementsByTagName('li')[0].id;
-
-
-
-
